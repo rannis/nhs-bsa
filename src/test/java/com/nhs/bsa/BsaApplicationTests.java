cd3fb93c0af99803f9f0b05d89bcf1b632e9bb57
remy <remyannis@hotmail.com>
2018-11-12 12:36:07 +0000

    NHS TEST added another test scenario.

--- src/test/java/com/nhs/bsa/BsaApplicationTests.java
+++ src/test/java/com/nhs/bsa/BsaApplicationTests.java
@@ -5,6 +5,7 @@ package com.nhs.bsa;
 //import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.test.context.junit4.SpringRunner;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,13 +17,14 @@ import javax.validation.Validation;
 import javax.validation.ValidatorFactory;
 import javax.validation.executable.ExecutableValidator;
 import java.lang.reflect.Constructor;
-import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class BsaApplicationTests {
 
     private ExecutableValidator executableValidator;
@@ -54,5 +56,25 @@ public class BsaApplicationTests {
 
         assertEquals(1, violations.size());
     }
+
+    @Test
+    public void whenCrossParameterValidationWithInvalidAmountAndAnyFrequencyConstructorParameters_thenZeroViolations() throws NoSuchMethodException {
+
+        Constructor<RegularAmount> constructor = RegularAmount.class.getConstructor(Amount.class, Frequency.class);
+
+        assertZeroViolationWhenZeroAmountAndAnyFrequency(constructor);
+    }
+
+    private void assertZeroViolationWhenZeroAmountAndAnyFrequency(Constructor<RegularAmount> constructor) {
+        Arrays.stream(Frequency.values()).forEach(frequency -> {
+            log.info(String.valueOf(frequency));
+            Object[] parameterValues = new Object[]{new Amount(0, 0), frequency};
+            Set<ConstraintViolation<RegularAmount>> violations =
+                    executableValidator.validateConstructorParameters(constructor, parameterValues);
+            assertEquals(0, violations.size());
+        });
+
+    }
+
 
 }
